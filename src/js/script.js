2ebd868c94af2f65f2f225b1290697fc36c3413c
Giovanna <giosay@hotmail.com>
2025-06-06 15:26:28 -0300

    Ajustando menu hamburguer

--- src/js/script.js
+++ src/js/script.js
@@ -48,3 +48,24 @@ function mudarCor(tema) {
 window.addEventListener('load', () => {
   slideShow();
 });
+
+//DECLARANDO AS VARIAVEIS
+
+const hamburguer =document.querySelector(".hamburguer");
+const headerMenu =document.querySelector(".header-menu");
+
+//CRIANDO A FUNÇÃO TOOGLE
+
+function toggleMenu(){
+    hamburguer.classList.toggle("active");
+    headerMenu.classList.toggle("active");
+}
+
+//CRIANDO O EVENTO 
+
+hamburguer.addEventListener('click',toggleMenu);
+headerMenu.addEventListener('click',(e)=>{
+    if(e.target.classList.contains('item-menu')){
+        toggleMenu();
+    }
+})
